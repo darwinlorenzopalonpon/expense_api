b7be7cac39c4828014d31ff25016bb91b5983bf9
Darwin P <darwinlorenzopalonpon@gmail.com>
2025-10-09 17:17:58 +0800

    Cleaned up configurations related to serving static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::API
-  def fallback_index_html
-    render file: Rails.public_path.join("index.html")
-  end
-
   private
 
   def set_current_user
--- config/application.rb
+++ config/application.rb
@@ -27,9 +27,6 @@ module ExpenseApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-
-    # Enable serving of static files for the frontend
-    config.api_only = false
-    config.public_file_server.enabled = true
+    config.api_only = true
   end
 end
